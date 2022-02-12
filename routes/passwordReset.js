ac4da7e0c219209f49620e460b35ad2c0904cc94
Dharanikumar19 <dharanikumarmuthusamy@gmail.com>
2022-02-13 03:28:29 +0530

    utu

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -52,34 +52,6 @@ router.get("/:id/:token", async (req, res) => {
 			userId: user._id,
 			token: req.params.token,
 		});
-		if (!token) return res.status(400).send({ message: "Invalid link" });
-
-		res.status(200).send("Valid Url");
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
-});
-
-//  set new password
-router.post("/:id/:token", async (req, res) => {
-	try {
-		const passwordSchema = Joi.object({
-			password: passwordComplexity().required().label("Password"),
-		});
-		const { error } = passwordSchema.validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
-
-		const user = await User.findOne({ _id: req.params.id });
-		if (!user) return res.status(400).send({ message: "Invalid link" });
-
-		const token = await Token.findOne({
-			userId: user._id,
-			token: req.params.token,
-		});
-		if (!token) return res.status(400).send({ message: "Invalid link" });
-
-		if (!user.verified) user.verified = true;
 
 		const salt = await bcrypt.genSalt(Number(process.env.SALT));
 		const hashPassword = await bcrypt.hash(req.body.password, salt);
